89df832fcc2137cdf66d3d6447f3816f36e25e43
Samuel S <ss202357052@alm.buap.mx>
2025-12-05 00:13:29 -0600

    Otra vez las tablas

--- dev_sistema_escolar_api/admin.py
+++ dev_sistema_escolar_api/admin.py
@@ -3,10 +3,10 @@ from django.utils.html import format_html
 from dev_sistema_escolar_api.models import *
 
 
-@admin.register(Administradores)
-@admin.register(Alumnos)
-@admin.register(Maestros)
-@admin.register(Eventos)
+admin.site.register(Alumnos)
+admin.site.register(Maestros)
+admin.site.register(Administradores)
+admin.site.register(Eventos)
 
 class ProfilesAdmin(admin.ModelAdmin):
     list_display = ("id", "user", "creation", "update")
